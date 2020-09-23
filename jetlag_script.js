6a067c9772aea325bd086e1d27a873411ef24f2f
Francesco <francesco.invidia.98@gmail.com>
2020-09-23 19:34:25 +0200

    animazione

--- jetlag_script.js
+++ jetlag_script.js
@@ -1,21 +1,22 @@
   // $('#hide').css("display","none")
   $('#hide').hide()
+  $('.hide').hide()
+
 function show(){
 $('#hide').toggle("show", function(){
   if ($('#hide').is(":hidden")) {
     $('.b_d').text("Read full info")
   }else{
     $('.b_d').text("Close full info")
-
   }
 })
+};
 
+function landing(){
+  $('.alliance').css("left", "0")
+  setTimeout(function(){
+    $('.hide').toggle("show");
+  },1300)
+};
 
-
-
-
-
-
-
-
-}
+setTimeout(landing,200)
